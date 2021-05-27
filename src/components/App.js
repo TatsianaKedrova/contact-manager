c602d5a3c8253b80a7101fcc0a8538ae4f18a6a1
tania <Baletrot@gmail.com>
2021-05-27 10:27:13 +0200

    created ui of header and addcontact components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import React from "react";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+import { v1 } from 'uuid';
 
 function App() {
+
+    const contacts = [
+        { id: v1(), name: "Tania", email: "baletro@mail.ru"},
+        { id: v1(), name: "Tatiana", email: "baletrot@gmail.com"}
+    ]
   return (
-    <div className="App">
+    <div className="ui container">
+        <Header />
+        <AddContact />
+        <ContactList />
 
     </div>
   );
